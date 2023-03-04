0801c3f969d92a0143d54c4b7337e615fe7e32cc
Budás Norbert <budasnorbert@gmail.com>
2023-03-04 15:17:30 +0100

    fixing getSongByID return type

--- node-addon/index.d.ts
+++ node-addon/index.d.ts
@@ -26,11 +26,76 @@ interface Changes {
   crossfader: number;
 }
 
-interface djmdContentRow {
+interface djmdContent {
   ID: string;
-  ContentID: string;
-  Cues: string;
-  rb_cue_count: number;
+  FolderPath: string;
+  FileNameL: string;
+  FileNameS: string;
+  Title: string;
+  ArtistID: string;
+  AlbumID: string;
+  GenreID: string;
+  BPM: number;
+  Length: number;
+  TrackNo: number;
+  BitRate: number;
+  BitDepth: number;
+  Commnt: string;
+  FileType: number;
+  Rating: number;
+  ReleaseYear: number;
+  RemixerID: string;
+  LabelID: string;
+  OrgArtistID: string;
+  KeyID: string;
+  StockDate: string;
+  ColorID: string;
+  DJPlayCount: number;
+  ImagePath: string;
+  MasterDBID: string;
+  MasterSongID: string;
+  AnalysisDataPath: string;
+  SearchStr: string;
+  FileSize: number;
+  DiscNo: number;
+  ComposerID: string;
+  Subtitle: string;
+  SampleRate: number;
+  DisableQuantize: number;
+  Analysed: number;
+  ReleaseDate: string;
+  DateCreated: string;
+  ContentLink: number;
+  Tag: string;
+  ModifiedByRBM: string;
+  HotCueAutoLoad: string;
+  DeliveryControl: string;
+  DeliveryComment: string;
+  CueUpdated: string;
+  AnalysisUpdated: string;
+  TrackInfoUpdated: string;
+  Lyricist: string;
+  ISRC: string;
+  SamplerTrackInfo: number;
+  SamplerPlayOffset: number;
+  SamplerGain: number;
+  VideoAssociate: string;
+  LyricStatus: number;
+  ServiceID: number;
+  OrgFolderPath: string;
+  Reserved1: string;
+  Reserved2: string;
+  Reserved3: string;
+  Reserved4: string;
+  ExtInfo: string;
+  rb_file_id: string;
+  DeviceID: string;
+  rb_LocalFolderPath: string;
+  SrcID: string;
+  SrcTitle: string;
+  SrcArtistName: string;
+  SrcAlbumName: string;
+  SrcLength: number;
   UUID: string;
   rb_data_status: number;
   rb_local_data_status: number;
@@ -44,5 +109,5 @@ interface djmdContentRow {
 
 declare module "rekordbox-reader" {
   export function trackChanges(callback: (changes: Changes) => void): void;
-  export function getSongByID(songId: number | string): Promise<any>;
+  export function getSongByID(songId: number | string): Promise<djmdContent>;
 }
